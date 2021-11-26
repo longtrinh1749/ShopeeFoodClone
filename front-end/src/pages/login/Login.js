36801c39e7ec298ccd2316cf5b7760c592ca6d10
Nguyen Thanh Tung <62226843+tunga2cvp@users.noreply.github.com>
2021-11-26 21:03:51 +0700

    testing authen api

--- front-end/src/pages/login/Login.js
+++ front-end/src/pages/login/Login.js
@@ -1,12 +1,36 @@
 import './Login.css'
+import axios from 'axios'
 const Login = () => {
+    function getAuthenInfo(){
+        // let username = document.getElementById("username");
+        // let password = document.getElementById("password");
+        // let authenInfo = {
+        //     "username" : username,
+        //     "password" : password
+        // }
+        axios.post('http://localhost:8762/user/auth', 
+        {
+            username: 'admin',
+            password: '12345'
+        })
+          .then( (response)=>{
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+          console.log("hi");
+    }
+    function sendAuthenInfo(){
+        
+    }
     return (
         <div className="wrapper">
             <div className="content-container">
                 <div className="title">Sign up</div>
                 <div className="login-via"> 
                     <div className="item phone">
-                        <span><i class="fas fa-mobile-alt"></i>SỐ ĐIỆN THOẠI</span>
+                        <span><i className="fas fa-mobile-alt"></i>SỐ ĐIỆN THOẠI</span>
                     </div>
                     <div className="item facebook">
                         <span><i className="fab fa-facebook-f"></i>FACEBOOK</span>
@@ -14,7 +38,7 @@ const Login = () => {
                     <div className="item google">
                         <span>
                             <i className="fab fa-google-plus-g" aria-hidden="true"></i>
-                            GOOGLE cai ri
+                            GOOGLE
                         </span>
                     </div>
                 </div>
@@ -23,21 +47,21 @@ const Login = () => {
                     <div className="field-group">
                         <div className="input-group">
                             <i className="far fa-envelope"></i>
-                            <input type="text" placeholder="Username or email"></input>
+                            <input type="text" placeholder="Username or email" id="username"></input>
                         </div>
                         <div className="input-group">
                             <i className="fas fa-lock"></i>
-                            <input type="password" placeholder="Password"></input>
+                            <input type="password" placeholder="Password" id="password"></input>
                         </div>
                     </div>
                     <div className="form-group clearfix">
                         <div className="float-left">
                             <input type="checkbox" id="RememberMe"></input>
-                            <label for="RememberMe">Lưu thông tin đăng nhập</label>
+                            <label>Lưu thông tin đăng nhập</label>
                         </div>
                     <span className="float-right"><a href="">Quên mật khẩu?</a></span>
                     </div>
-                    <button className="btn btn-block btn-submit">ĐĂNG NHẬP</button>
+                    <button className="btn btn-block btn-submit" onClick={getAuthenInfo}>ĐĂNG NHẬP</button>
                     <div className="login-mess-policy">
                         Chúng tôi không sử dụng thông tin của bạn với bất kỳ mục đích nào. Bằng cách đăng nhập hoặc đăng ký, bạn đồng ý với 
                         <a target="_blank" href="https://shopeefood.vn/gioi-thieu#footer-bottom">  Chính sách quy định của Foody</a>
