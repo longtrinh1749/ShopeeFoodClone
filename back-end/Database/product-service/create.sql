ae7a8ede7a80752682a7a28a4358c60f0e07d958
Đỗ Tiến Thành <thanh21.cnk@gmail.com>
2021-11-29 23:13:44 +0700

    Revert "change database script"
    This reverts commit 00c5f9968658eac986a069210762dd17b0667340.

--- back-end/Database/product-service/create.sql
+++ back-end/Database/product-service/create.sql
@@ -5,7 +5,7 @@ use `product_service`;
 create table `shop` (
     `id` int auto_increment primary key,
     `name` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci not null,
-    `img_url` varchar(500) not null,
+    `img_url` varchar(500),
     `address` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci not null,
     `district` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci not null,
     `city` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci not null,
@@ -21,8 +21,8 @@ create table `shop_category` (
     `shop_id` int,
     `category_id` int,
     primary key (`shop_id`, `category_id`),
-    foreign key (`shop_id`) references `shop`(`id`),
-    foreign key (`category_id`) references `category`(`id`)
+    foreign key (`shop_id`) references shop(id),
+    foreign key (`category_id`) references category(id)
 );
 
 create table `section` (
@@ -30,24 +30,22 @@ create table `section` (
     `shop_id` int,
     `name` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci not null,
     foreign key (`shop_id`) references `shop`(`id`)
-);
+)
 
 create table `item` (
     `id` int auto_increment primary key,
     `section_id` int not null,
     `name` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci not null,
-    `img_url` varchar(255) not null,
-    `description` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+    `img_url` varchar(255),
+    `description` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci varchar(500),
     `price` decimal(16,0) not null,
     `sale` int not null,
-    `status` bit(1) not null,
-    foreign key (`section_id`) references `section`(`id`)
-);
+    `status` bit(1) not null
+)
 
 create table `item_audit` (
-    `id` int primary key,
+    `id` int auto_increment primary key,
     `update_at` datetime not null,
-    `price` decimal(16,0) not null,
-    `sale` int not null,
-    foreign key (`id`) references `item`(`id`)
-);
+    `price` int not null,
+    `sale` int not null
+)
