2fba4f49a2223a43ba048eea65f97da6b4773c99
Long <leduclong0101@gmail.com>
2021-12-17 20:43:44 +0700

    merge with main

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,37 +1,9 @@
 import "./app.scss";
 import "bootstrap";
 import Header from "./components/Header/Header";
-// import Home from "./pages/home/templates/Home/Home";
+import Home from "./pages/home/templates/Home/Home";
 import Footer from "./components/Footer/Footer";
-<<<<<<< HEAD
-// import Search from "./pages/search/Search";
-// import ItemShop from "./components/ItemShop/ItemShop";
-import Shop from "./pages/shop/shop"
-
-
-=======
 import Search from "./pages/search/Search";
-<<<<<<< HEAD
-import Login from "./pages/login/Login"
->>>>>>> bc60affb109376268b0cd5c1a9b861d0b2138830
-function App() {
-    return (
-        <>
-            <Header />
-            <div className="wrapper">
-                {/* <Home /> */}
-<<<<<<< HEAD
-                {/* <ItemShop item={ itemShop }/> */}
-                <Shop />
-=======
-                {/* <Search /> */}
-                <Login/>
->>>>>>> bc60affb109376268b0cd5c1a9b861d0b2138830
-            </div>
-            <Footer />
-        </>
-    );
-=======
 import Login from "./pages/login/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React, { useState } from 'react';
@@ -57,7 +29,6 @@ function App() {
       <Footer />
     </>
   );
->>>>>>> 5adf50c11ddacd2f396139bf153d376c4fd27a28
 }
 
 export default App;
