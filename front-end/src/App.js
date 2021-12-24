dde198a6f768c11992b31196d714650a4c870001
Long <leduclong0101@gmail.com>
2021-12-24 22:18:15 +0700

    commit code

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,6 +8,7 @@ import Login from "./pages/login/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React, { useState } from 'react';
 import Signup from "./pages/signup/Signup";
+import Shop from "./pages/shop/shop";
 
 function App() {
   const [user, setUser] = useState('');
@@ -24,6 +25,7 @@ function App() {
             <Route path="login" element={<Login setUser={setUser}/>} />
             <Route path="search" element={<Search />} />
             <Route path="signup" element = {<Signup/>} />
+            <Route path="shop" element = {<Shop/>} />
         </Routes>
       </BrowserRouter>
       <Footer />
--- front-end/src/components/ItemShop/ItemShop.js
+++ front-end/src/components/ItemShop/ItemShop.js
@@ -1,26 +1,45 @@
+import { Container, Col, Row } from "react-bootstrap";
 import "./itemShop.scss";
 
 const ItemShop = ({ item }) => {
+    function addToCart() {
+        // foodObj['quantity'] = parseInt(document.getElementById("quantity-mobiles").value);
+        // //console.log(foodObj);
+        // var mobiles = [];
+        // if (sessionStorage.getItem("mobiles") !== null) {
+        //     mobiles = JSON.parse(sessionStorage.getItem("mobiles"));
+        //     //console.log(mobiles);
+        //     var tmpMobile = mobiles.find(mobile => mobile['id'] == foodObj['id']);
+        //     if (tmpMobile) {
+        //         tmpMobile['quantity'] += foodObj['quantity'];
+        //     } else {
+        //         mobiles.push(foodObj);
+        //     }
+        // } else {
+        //     mobiles.push(foodObj);
+        // }
+        // sessionStorage.setItem("mobiles", JSON.stringify(mobiles));
+        // alert("Successfully added to cart!");
+    }
     return (
-        <div className="item">
-            <a href="/">
-                <div className="item_img">
-                    <img src={item.image} alt="" />
-                </div>
-                <div className="item_info_shop">
-                    <div className="item_info_shop-title">{item.title}</div>
-                </div>
-                <div className="item_discount">
-                    <span className="discount-price">{item.discount}</span>
-                    <span className="currency-unit-discount">đ</span>
-                    <span className="price">{item.price}</span>
-                    <span className="currency-unit">đ</span>
-                </div>
-                <div className="btn-add-to-cart">
-                    <div className="btn-add">+</div>
-                </div>
-                {item.active && <div className="item_active"></div>}
-            </a>
+        <div>
+            <Container className="item-shop">
+                <Row>
+                    <Col className="item-shop-image">
+                        <img src="/images/Logo-ShopeefoodVN.png" alt="" width={60} height={60}></img>
+                    </Col>
+                    <Col md={8} className="item-shop-name">
+                        <Row className="item-shop-itemname">{item.itemName}</Row>
+                    </Col>
+                    <Col className="item-shop-price">
+                        <Row className="old-price">{item.price}đ</Row>
+                        <Row className="new-price">{item.price}đ</Row>
+                    </Col>
+                    <Col md={1} className="item-shop-add-btn">
+                        <span className="btn-add-to-cart" onClick={addToCart}>+</span>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 };
