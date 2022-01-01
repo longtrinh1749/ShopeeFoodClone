eec06b61e9db281664667f1225de2e5a1ee269c7
longtrinh1749 <trinhlong1749@gmail.com>
2022-01-02 05:05:18 +0700

    update db shop-owner 1-1 relationship, items in order belong to 1 shop

--- back-end/Database/product-service/script.py
+++ back-end/Database/product-service/script.py
@@ -2,9 +2,11 @@ import io
 import random
 with io.open('product-service/tmp.txt','r',encoding='utf8') as f:
     lines = f.readlines()
+    i = 100
     for line in lines:
+        i += 1
         line = line.split('values (')
-        int = random.randint(51,100)
+        int = i
         line[0] = line[0] + f"values ('{int}', "
         line = line[0] + line[1]
         print(line)
